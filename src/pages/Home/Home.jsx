7293fbfc996dd623d7a7ab2258870d16cfa40b80
Marcos <marcoschavezwebmail.com>
2026-04-10 12:02:15 -0300

    Creacion de formulario y funcion añadir pelicula

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,16 @@ import { useState, useEffect } from "react";
 
 // Componentes
 import Titulo from "../../components/titulo/Titulo";
-
+import Formulario from "../../components/formulario/Formulario";
 
 const Home = () => {
-    
-    
+
+    // UseStates
+    const [modalFormulario, setModalFormulario] = useState(false)
+
+    const [modalEditar, setModalEditar] = useState(false)
+
+
     // Obtener datos del local storage
     const obtenerPeliculas = () => {
         const data = localStorage.getItem("peliculas");
@@ -17,6 +22,18 @@ const Home = () => {
 
     const [peliculas, setPeliculas] = useState(obtenerPeliculas);
 
+    
+    // Función para agregar película
+    const agregarPelicula = (nuevaPelicula) => {
+        const peliculasActualizadas = [...peliculas, nuevaPelicula];
+        setPeliculas(peliculasActualizadas);
+        localStorage.setItem("peliculas", JSON.stringify(peliculasActualizadas));
+    };
+
+    // Función para cerrar modal
+    const cerrarModal = () => {
+        setModalFormulario(false);
+    };
 
 
     // PARA LAUTI QUE VA A CREAR LAS FUNCIONES
@@ -28,28 +45,34 @@ const Home = () => {
 
   return (
     <div>
-      <Titulo texto="Gestor de Películas y Series" />
+        <Titulo texto="Gestor de Películas y Series" />
 
-      {/* Formulario */}
-      {/* <Formulario /> */}
+        <button onClick={() => setModalFormulario(true)}>
+            Agregar Película o Serie
+        </button>
 
-      {/* Filtros */}
-      {/* <Filtros /> */}
+        {/* Contadores */}
+        <div>
+            <p>Por ver: 0</p>
+            <p>Vistas: 0</p>
+        </div>
 
-      {/* Contadores */}
-      <div>
-        <p>Por ver: 0</p>
-        <p>Vistas: 0</p>
-      </div>
+        {/* Listas */}
+        <h2>Por ver</h2>
+        {/* <Lista tipo="porVer" /> */}
+        <p>No hay películas por ver</p>
 
-      {/* Listas */}
-      <h2>Por ver</h2>
-      {/* <Lista tipo="porVer" /> */}
-      <p>No hay películas por ver</p>
+        <h2>Vistas</h2>
+        {/* <Lista tipo="vistas" /> */}
+        <p>No hay películas vistas</p>
 
-      <h2>Vistas</h2>
-      {/* <Lista tipo="vistas" /> */}
-      <p>No hay películas vistas</p>
+
+        {/* Modal del formulario */}
+        <Formulario 
+            agregar={agregarPelicula}
+            cerrar={cerrarModal}
+            mostrar={modalFormulario}
+        />
     </div>
   );
 }
